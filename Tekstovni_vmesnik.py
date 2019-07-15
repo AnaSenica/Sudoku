0716175b30e8d75bbf073c08b265aada05fa9a80
AnaSenica <ana.senica1999@gmail.com>
2019-07-15 12:54:42 +0200

    Dokončala tekstovni vmesnik

--- Tekstovni_vmesnik.py
+++ Tekstovni_vmesnik.py
@@ -2,14 +2,14 @@ import model
 
 stevila = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-
 def pozeni_vmesnik():
     #zahtevaj izbiro težavnosti:
     stevilo = tezavnost()
     if stevilo == False:
-        print('Napaka. Vpišite: 1, 2 ali 3')
+        print('Napaka. Vpišite 1, 2 ali 3, da izberete težavnost.')
         return pozeni_vmesnik()
     else:
+        print('='*52 + '\n\n')
         igra = model.nova_igra(stevilo)
         while True:
             #izpišem igro:
@@ -20,20 +20,26 @@ def pozeni_vmesnik():
             stevilka = izberi_stevilko()
             ugib = igra.ugibaj(stevilka, vrsta, stolpec)
             if ugib == 'Zmaga.':
+                print('='*52)
+                print('\nČestitam, uspešno ste rešili sudoku!\n')
                 izpisi_zmago()
                 break
             elif ugib == 'Napacno.':
-                print('Napačno, ugibajte še enkrat.')
+                print('='*52)
+                print('\nNAPAČNO, ugibajte še enkrat:\n')
                 pass
             elif ugib == 'Napaka 2.':
-                print('Že obstoječe mreže ne morete spreminjati.')
+                print('='*52)
+                print('\nPOZOR: Vnaprej napisanih številk ne morete spreminjati.\nPoskusite z drugim poljem:\n')
                 pass
+            elif ugib == 'Pravilno.':
+                print('='*52)
+                print('\nPRAVILNO. Ugibajte naprej:\n')
         return None
 
 
-
 def izberi_stevilko():
-    stevilka = input('\nIzberite stevilko: 1-9: ')
+    stevilka = input('\nIzberite stevilko: 1-9 (Vtipkajte število od 1 do 9 in pritisnite ENTER): ')
     if stevilka not in stevila:
         print('\nVtipkajte število od 1 do 9.')
         return izberi_stevilko()
@@ -42,7 +48,7 @@ def izberi_stevilko():
 
 
 def izberi_vrsto():
-    vrsta = input('\nIzberite vrsto 1-9: ')
+    vrsta = input('\nIzberite vrsto 1-9 (Vtipkajte število od 1 do 9 in pritisnite ENTER): ')
     indeks = None
     if vrsta not in stevila:
         print('\nVtipkajte število od 1 do 9.')
@@ -52,10 +58,10 @@ def izberi_vrsto():
         return indeks
 
 def izberi_stolpec():
-    stolpec = input('\nIzberite vrsto 1-9: ')
+    stolpec = input('\nIzberite vrsto 1-9 (Vtipkajte število od 1 do 9 in pritisnite ENTER): ')
     indeks = None
     if stolpec not in stevila:
-        print('\nVtipkajte število od 1 do 9.')
+        print('\nPOZOR: Vtipkati morate število od 1 do 9.')
         return izberi_stolpec()
     else:
         indeks = int(stolpec) - 1
@@ -64,15 +70,24 @@ def izberi_stolpec():
 
 def izpis_igre(igra):
     sudoku = igra.sudoku
+    print('VRSTE\STOLPCI:', end = '')
+    print('   1   2   3   4   5   6   7   8   9')
+    print('-'*52)
+    stevilka_vrste = 1
     for vrsta in sudoku:
-        print('')
+        print(' '*9, end = '')
+        print(stevilka_vrste, end = '')
+        stevilka_vrste += 1
+        print('     | ', end = '')
         for stevilka in vrsta:
-            print(stevilka, end ='')
-            print(' ', end ='')
+            print(stevilka, end =' | ')
+        print('')
+        
+        
 
 
 def izberi_tezavnost():
-    return input('Izberite težavnost: (1)lahko, (2)srednje težko, (3)težko: ')
+    return input('Izberite težavnost:\n(1)lahko\n(2)srednje težko\n(3)težko:\n')
 
 def tezavnost():
     tezavnost = izberi_tezavnost()
@@ -87,7 +102,24 @@ def tezavnost():
 
 
 def izpisi_zmago():
-    print('Čestitam, uspešno ste rešili sudoku!')
+    odgovor = se_enkrat()
+    if odgovor == False:
+        print('Napaka. Vpišite 1 ali 2, da izberete svoje naslednje dejanje.')
+        return izpisi_zmago()
+
+
+
+def igraj_se_enkrat():
+    return input('\nŽelite igrati še enkrat?\n1) Da.\n2) Ne.\n')
+    
+def se_enkrat():
+    igra = igraj_se_enkrat()
+    if igra == '1':
+        return pozeni_vmesnik()
+    elif igra == '2':
+        print('\nHvala, da ste igrali igro. Lep dan še naprej!')
+    else:
+        return False
 
 
 
